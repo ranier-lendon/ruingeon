98481e9e389cb6fdfc47fe0e4f849cf3138e929a
Lendon <ranierlendon11@gmail.com>
2026-09-20 23:08:00 +0800

    - scenes of each room

--- scripts/MapBuilder.cs
+++ scripts/MapBuilder.cs
@@ -8,10 +8,18 @@ public partial class MapBuilder : Node3D
 {
     private static int size = 9;
     private static char[,] map;
-    public override void _Ready()
 
     // Scenes
-    PackedScene normal = GD.Load<PackedScene>("res://scenes/dungeon rooms/bridge.tscn");
+    PackedScene boss = GD.Load<PackedScene>("res://scenes/dungeonRooms/boss.tscn");
+    PackedScene bridge = GD.Load<PackedScene>("res://scenes/dungeonRooms/bridge.tscn");
+    PackedScene elite = GD.Load<PackedScene>("res://scenes/dungeonRooms/elite.tscn");
+    PackedScene heal = GD.Load<PackedScene>("res://scenes/dungeonRooms/heal.tscn");
+    PackedScene normal = GD.Load<PackedScene>("res://scenes/dungeonRooms/normal.tscn");
+    PackedScene portal = GD.Load<PackedScene>("res://scenes/dungeonRooms/portal.tscn");
+    PackedScene shop = GD.Load<PackedScene>("res://scenes/dungeonRooms/shop.tscn");
+    PackedScene treasure = GD.Load<PackedScene>("res://scenes/dungeonRooms/treasure.tscn");
+    
+    public override void _Ready()
     {
         map = MapSystem.GenerateMap(1);
     }
