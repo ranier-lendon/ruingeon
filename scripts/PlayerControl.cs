aa25d24c18e5d9c94083acfa96640c457d1c8bdd
Lendon <ranierlendon11@gmail.com>
2026-09-22 01:04:45 +0800

    player look at mouse

--- scripts/PlayerControl.cs
+++ scripts/PlayerControl.cs
@@ -6,6 +6,15 @@ public partial class PlayerControl : CharacterBody3D
     public const float Speed = 10.0f;
     public const float JumpVelocity = 4.5f;
 
+    private Node3D _visuals;
+    private Camera3D _camera;
+
+    public override void _Ready()
+    {
+        _visuals = GetNode<Node3D>("Visuals");
+        _camera = GetViewport().GetCamera3D();
+    }
+
     public override void _PhysicsProcess(double delta)
     {
         Vector3 velocity = Velocity;
@@ -27,5 +36,28 @@ public partial class PlayerControl : CharacterBody3D
 
         Velocity = velocity;
         MoveAndSlide();
+
+        // Rotate Visuals to face the mouse cursor on the XZ plane
+        if (_visuals != null && _camera != null)
+        {
+            Vector2 mousePos = GetViewport().GetMousePosition();
+            Vector3 rayOrigin = _camera.ProjectRayOrigin(mousePos);
+            Vector3 rayDir = _camera.ProjectRayNormal(mousePos);
+
+            // Intersect ray with the horizontal plane at the player's Y position
+            float planeY = GlobalPosition.Y;
+            if (Mathf.Abs(rayDir.Y) > 0.001f)
+            {
+                float t = (planeY - rayOrigin.Y) / rayDir.Y;
+                Vector3 worldPoint = rayOrigin + rayDir * t;
+
+                Vector3 toMouse = worldPoint - GlobalPosition;
+                if (toMouse.LengthSquared() > 0.001f)
+                {
+                    float angle = Mathf.Atan2(toMouse.X, toMouse.Z);
+                    _visuals.Rotation = new Vector3(_visuals.Rotation.X, angle, _visuals.Rotation.Z);
+                }
+            }
+        }
     }
 }
