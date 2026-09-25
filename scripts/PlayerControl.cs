a613a93c7d6d7e439f05c9f2bee1d848312461ed
Lendon <ranierlendon11@gmail.com>
2026-09-25 20:12:33 +0800

    removes extra code in physicsProcess

--- scripts/PlayerControl.cs
+++ scripts/PlayerControl.cs
@@ -63,28 +63,5 @@ public partial class PlayerControl : CharacterBody3D
 
         Velocity = velocity;
         MoveAndSlide();
-
-        // Rotate Visuals to face the mouse cursor on the XZ plane
-        if (_visuals != null && _camera != null)
-        {
-            Vector2 mousePos = GetViewport().GetMousePosition();
-            Vector3 rayOrigin = _camera.ProjectRayOrigin(mousePos);
-            Vector3 rayDir = _camera.ProjectRayNormal(mousePos);
-
-            // Intersect ray with the horizontal plane at the player's Y position
-            float planeY = GlobalPosition.Y;
-            if (Mathf.Abs(rayDir.Y) > 0.001f)
-            {
-                float t = (planeY - rayOrigin.Y) / rayDir.Y;
-                Vector3 worldPoint = rayOrigin + rayDir * t;
-
-                Vector3 toMouse = worldPoint - GlobalPosition;
-                if (toMouse.LengthSquared() > 0.001f)
-                {
-                    float angle = Mathf.Atan2(toMouse.X, toMouse.Z);
-                    _visuals.Rotation = new Vector3(_visuals.Rotation.X, angle, _visuals.Rotation.Z);
-                }
-            }
-        }
     }
 }
